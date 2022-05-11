b43c0b779888f2d2582cb609580517d6f8833346
coded4game <jeff.d4.hemphill@gmail.com>
2022-05-11 17:16:07 -0500

    Completed - Use Unit Testing to Verify Parts of the Job Class.

---
+++ src/org/launchcode/techjobs_oo/tests/JobTest.java
@@ -0,0 +1,119 @@
+package org.launchcode.techjobs_oo.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    // Declared Job objects
+    Job test_job1;
+    Job test_job2;
+    Job test_job3;
+    Job test_job4;
+    Employer employer;
+    Location location;
+    PositionType positionType;
+    CoreCompetency coreCompetency;
+
+    @Before
+    public void createJobObj() {
+
+        // Initialized job objects
+        test_job1 = new Job(
+                "Product tester",
+                employer = new Employer("ACME"),
+                location = new Location("Desert"),
+                positionType = new PositionType("Quality control"),
+                coreCompetency = new CoreCompetency("Persistence")
+        );
+        test_job2 = new Job(
+                "Job Test2",
+                employer = new Employer("ACME"),
+                location = new Location("Desert"),
+                positionType = new PositionType("Quality control"),
+                coreCompetency = new CoreCompetency("Persistence")
+        );
+        test_job3 = new Job(
+                "Product tester",
+                employer = new Employer("ACME"),
+                location = new Location("Desert"),
+                positionType = new PositionType("Quality control"),
+                coreCompetency = new CoreCompetency("Persistence")
+        );
+        test_job4 = new Job(
+                "",
+                new Employer(""),
+                new Location(""),
+                new PositionType(""),
+                new CoreCompetency("")
+        );
+    }
+
+    @Test
+    public void emptyTest() {
+        assertEquals(1, 1, .001);
+    }
+
+    // Test the Empty Constructor
+    @Test
+    public void testSettingJobId() {
+        test_job1.getId();
+        assertTrue(test_job1.getId() != test_job2.getId());
+
+        test_job2.getId();
+        Integer difference = test_job1.getId() - test_job2.getId();
+        assertTrue(difference == -1);
+    }
+
+    // Test the Full Constructor
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertTrue(test_job3 instanceof Job);
+        assertEquals(test_job3.getName(), "Product tester");
+        assertEquals(test_job3.getEmployer(), employer);
+        assertEquals(test_job3.getLocation(), location);
+        assertEquals(test_job3.getPositionType(), positionType);
+        assertEquals(test_job3.getCoreCompetency(), coreCompetency);
+    }
+
+    // Test the equals Method
+    @Test
+    public void testJobsForEquality() {
+        assertFalse(test_job1.equals(test_job3));
+    }
+
+    @Test
+    public void testJobToStringNewlines() {
+        String output = test_job3.toString();
+        assertTrue(output.startsWith("\n"));
+        assertTrue(output.endsWith("\n"));
+    }
+
+    @Test
+    public void testFirstToString() {
+        String toStringFirstTest = "" + "\n" +
+                "ID: " + test_job3.getId() + "\n" +
+                "Name: " + test_job3.getName() + "\n" +
+                "Employer: " + test_job3.getEmployer() + "\n" +
+                "Location: " + test_job3.getLocation() + "\n" +
+                "Position Type: " + test_job3.getPositionType() + "\n" +
+                "Core Competency: " + test_job3.getCoreCompetency() + "\n" +
+                "";
+        assertEquals(toStringFirstTest, test_job3.toString());
+    }
+
+    @Test
+    public void testSecondToString() {
+        String toStringSecondTest = "" + "\n" +
+                "ID: " + test_job4.getId() + "\n" +
+                "Name: " + "Data not available" + "\n" +
+                "Employer: " + "Data not available" + "\n" +
+                "Location: " + "Data not available" + "\n" +
+                "Position Type: " + "Data not available" + "\n" +
+                "Core Competency: " + "Data not available" + "\n" +
+                "";
+        assertEquals(toStringSecondTest, test_job4.toString());
+    }
+}
